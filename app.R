4b8d16404b3a0578a6a0c97ccdee885b5d4cbd96
iman-msv <80585557+iman-msv@users.noreply.github.com>
2023-09-25 14:15:31 +0330

    Line Plots

--- app.R
+++ app.R
@@ -3,7 +3,7 @@ if (!require(pacman)) {
   library(pacman)
 }
 
-p_load(shiny, shinydashboard, readr, dplyr, ggplot2, stringr, magrittr,
+p_load(shiny, shinydashboard, readr, dplyr, ggplot2, plotly, stringr, magrittr,
        lubridate)
 
 walmart <- read_csv("Walmart_Store_sales.csv")
@@ -16,7 +16,8 @@ names(walmart) %<>%
 walmart %<>%
   mutate(date = dmy(date),
          store = as.factor(store),
-         holiday_flag = as.factor(holiday_flag))
+         holiday_flag = as.factor(holiday_flag),
+         weekly_sales = weekly_sales / 1e3)
 
 # Dashboard
 # Header, Sidebar, and Body
@@ -36,7 +37,11 @@ body <- dashboardBody(
     tabItem(
       "dashboard",
       fluidRow(
-        valueBoxOutput("age")
+        valueBoxOutput("mean_sales"),
+        valueBoxOutput("num_weeks")
+      ),
+      fluidRow(
+        plotlyOutput("sales_over_weeks_store1")
       )
     ),
     tabItem(
@@ -57,15 +62,38 @@ server <- function(input, output) {
     walmart
   )
   
-  output$age <- renderValueBox({
+  output$mean_sales <- renderValueBox({
     mean <- round(mean(walmart$weekly_sales), 2)
-    
     valueBox(
       value = mean,
-      subtitle = "Weekly Sales Average",
-      icon = icon("image-portrait"),
+      subtitle = "Weekly Sales Average (thousand $)",
+      icon = icon("dollar-sign"),
       color = "maroon"
     )
+  })
+    
+  output$num_weeks <- renderValueBox({
+    num_unique_weeks <- length(unique(walmart$date))
+    valueBox(
+      value = num_unique_weeks,
+      subtitle = "Weeks",
+      icon = icon("calendar"),
+      color = "maroon"
+    )
+  })
+  
+  output$sales_over_weeks_store1 <- renderPlotly({
+    sales_store1_plot <- walmart |> 
+      ggplot(aes(date, weekly_sales, group = store)) + 
+      geom_line(aes(color = store), alpha = 0.8) + 
+      labs(
+        x = "Date", 
+        y = "Weekly Sales (thousand $)", 
+        title = "Weekly Sales over Time in Store 1"
+      ) + 
+      theme(legend.position = "none")
+      
+      ggplotly(sales_store1_plot)
   })
 }
 
